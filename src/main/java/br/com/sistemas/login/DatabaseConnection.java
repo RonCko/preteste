7e2ddd5a31fd780aca219af5bca273ad0f4f54b5
Kaylany Rafaela Martins de Paula <kpaula2101@hotmail.com>
2025-01-25 18:58:53 -0300

    rafa

--- src/main/java/br/com/sistemas/login/DatabaseConnection.java
+++ src/main/java/br/com/sistemas/login/DatabaseConnection.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/Reteste";
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/teste";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
 
